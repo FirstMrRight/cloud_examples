9a1371e89b47e0ac5bbf119f229b0879837121c5
Liutx <15666882775@163.com>
2021-05-16 22:20:17 +0800

    Initial commit

---
+++ consumer/src/main/java/com/example/demo/SonsumerServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonsumerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SonsumerServerApplication.class, args);
+    }
+
+}
---
+++ eureka_server/src/test/java/com/liutx/demo/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liutx.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

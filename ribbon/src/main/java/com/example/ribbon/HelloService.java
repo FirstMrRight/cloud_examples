8b0d1452da7372497d40bd11eccf58e0ac7053c0
Liutx <15666882775@163.com>
2021-05-19 20:38:31 +0800

    Service接口

--- ribbon/src/main/java/com/example/ribbon/HelloService.java
+++ ribbon/src/main/java/com/example/ribbon/HelloService.java
@@ -11,7 +11,7 @@ public class HelloService {
     RestTemplate restTemplate;
 
     public String hiService(String name) {
-        return name;
+        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
     }
 
 
